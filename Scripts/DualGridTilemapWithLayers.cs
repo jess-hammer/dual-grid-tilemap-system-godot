02ed45371e7597def637dceee638b50b7a8da1b4
SonarBeserk <sonarbeserk@gmail.com>
2025-07-26 20:29:47 -0400

    Rewrite DualGridTilemapWithLayers for TileMapLayers

--- Scripts/DualGridTilemapWithLayers.cs
+++ Scripts/DualGridTilemapWithLayers.cs
@@ -3,9 +3,9 @@ using System;
 using System.Collections.Generic;
 using static TileType;
 
-public partial class DualGridTilemapWithLayers : TileMap {
-    public static int MAIN_LAYER = 0;
-    public static int DISPLAY_LAYER = 1;
+public partial class DualGridTilemapWithLayers : Node2D {
+    [Export] public TileMapLayer mainLayer;
+    [Export] public TileMapLayer displayLayer;
     [Export] public Vector2I grassPlaceholderAtlasCoord;
     [Export] public Vector2I dirtPlaceholderAtlasCoord;
     readonly Vector2I[] NEIGHBOURS = new Vector2I[] { new(0, 0), new(1, 0), new(0, 1), new(1, 1) };
@@ -31,13 +31,21 @@ public partial class DualGridTilemapWithLayers : TileMap {
 
     public override void _Ready() {
         // Refresh all display tiles
-        foreach (Vector2I coord in GetUsedCells(MAIN_LAYER)) {
+        foreach (Vector2I coord in mainLayer.GetUsedCells()) {
             setDisplayTile(coord);
         }
     }
 
+    /// <summary>
+    /// <para>Returns the map coordinates of the cell containing the given <paramref name="localPosition"/>. If <paramref name="localPosition"/> is in global coordinates, consider using <see cref="Godot.Node2D.ToLocal(Vector2)"/> before passing it to this method. See also <see cref="Godot.TileMapLayer.MapToLocal(Vector2I)"/>.</para>
+    /// </summary>
+    public Vector2I LocalToMap(Vector2 pos)
+    {
+        return mainLayer.LocalToMap(pos);
+    }
+
     public void SetTile(Vector2I coords, Vector2I atlasCoords) {
-        SetCell(0, coords, 0, atlasCoords);
+        mainLayer.SetCell(coords, 0, atlasCoords);
         setDisplayTile(coords);
     }
 
@@ -45,16 +53,16 @@ public partial class DualGridTilemapWithLayers : TileMap {
         // loop through 4 display neighbours
         for (int i = 0; i < NEIGHBOURS.Length; i++) {
             Vector2I newPos = pos + NEIGHBOURS[i];
-            SetCell(DISPLAY_LAYER, newPos, 0, calculateTile(newPos));
+            displayLayer.SetCell(newPos, 0, calculateTile(newPos));
         }
     }
 
     Vector2I calculateTile(Vector2I coords) {
         // get 4 world tile neighbours
-        TileType botRight = getTileType(GetCellAtlasCoords(0, coords - NEIGHBOURS[0]));
-        TileType botLeft = getTileType(GetCellAtlasCoords(0, coords - NEIGHBOURS[1]));
-        TileType topRight = getTileType(GetCellAtlasCoords(0, coords - NEIGHBOURS[2]));
-        TileType topLeft = getTileType(GetCellAtlasCoords(0, coords - NEIGHBOURS[3]));
+        TileType botRight = getTileType(mainLayer.GetCellAtlasCoords(coords - NEIGHBOURS[0]));
+        TileType botLeft = getTileType(mainLayer.GetCellAtlasCoords(coords - NEIGHBOURS[1]));
+        TileType topRight = getTileType(mainLayer.GetCellAtlasCoords(coords - NEIGHBOURS[2]));
+        TileType topLeft = getTileType(mainLayer.GetCellAtlasCoords(coords - NEIGHBOURS[3]));
 
         // return tile (atlas coord) that fits the neighbour rules
         return neighboursToAtlasCoord[new(topLeft, topRight, botLeft, botRight)];
